f9ec8efaa79fc0d6a28dd8425613bc0d52420062
harris012 <harissaeed@protonmail.com>
2022-05-09 00:37:05 +0200

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Resume v7.0.4 (https://harris012.github.io/portfolio)
+* Start Bootstrap - Resume v7.0.5 (https://harris012.github.io/portfolio)
 * Copyright 2013-2022 undefined
 * Licensed under undefined (https://github.com/StartBootstrap/resume/blob/master/LICENSE)
 */
